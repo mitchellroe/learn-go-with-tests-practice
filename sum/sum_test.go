da07f5adbdbd7f88cb2dd16d582aa26ed54a196b
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-11 19:50:02 -0500

    Discovered the "reflect" library. Using to compare slices

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -12,6 +15,15 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got '%d' want '%d', given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("test multiple summations", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
+		want := []int{6, 15}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 
