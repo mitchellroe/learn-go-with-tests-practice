573950daeca7521b6f4934b687f1eac3cb35952a
Mitchell Roe <mbroe@oakland.edu>
2020-02-14 18:09:28 -0500

    Added a new test for area

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -1,4 +1,4 @@
-package perimeter
+package geometry
 
 import (
 	"testing"
@@ -7,6 +7,15 @@ import (
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
--- perimeter/perimeter.go
+++
@@ -1,5 +0,0 @@
-package perimeter
-
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
-}
