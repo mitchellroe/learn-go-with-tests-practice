7643ee1b37d51eff68eda171c708498ac943e8f7
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-16 15:56:20 -0500

    Added a triangle

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -30,6 +30,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Height * t.Base) / 2
+}
+
 // Perimeter returns the perimeter of a rectangle
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -25,6 +25,7 @@ func TestArea(t *testing.T) {
 		// And here's a list of the ad-hoc structs that go in our slice.
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
