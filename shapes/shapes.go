e02ab9799ffe5bc12558c05055bed0a1644ad9a9
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-16 15:21:13 -0500

    Adding comments to the linter will stop complaining

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -4,19 +4,23 @@ import (
 	"math"
 )
 
+// Rectangle defines a rectangle
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+// Area returns the area of a rectangle r
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+// Circle defines a circle
 type Circle struct {
 	Radius float64
 }
 
+// Area returns the area of a circle c
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
