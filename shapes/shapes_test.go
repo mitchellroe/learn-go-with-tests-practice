229325210b13057f213fa4c96bf64dac201a5e83
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-16 15:43:34 -0500

    Wow. That's really cool.
    We created a slice of ad-hoc structs, which contained a Shape and a float64,
    then we just iterated over that slice and ran the tests that we wanted to run.
    
    That's neat.

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -16,22 +16,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	// areaTests is a slice of structs
+	areaTests := []struct {
+		// and the structs that will go in it have a Shape and a float64
+		shape Shape
+		want  float64
+	}{
+		// And here's a list of the ad-hoc structs that go in our slice.
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 
 }
