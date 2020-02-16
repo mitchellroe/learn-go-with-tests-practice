974ce45180d61c76a83a2cdf4a94a5e637aee5a0
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-16 16:58:10 -0500

    Removed comments

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -16,13 +16,10 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	// areaTests is a slice of structs
 	areaTests := []struct {
-		// and the structs that will go in it have a Shape and a float64
 		shape Shape
 		want  float64
 	}{
-		// And here's a list of the ad-hoc structs that go in our slice.
 		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
 		{shape: Circle{Radius: 10}, want: 314.1592653589793},
 		// {shape: Triangle{Base: 12, Height: 6}, want: 36.0},
