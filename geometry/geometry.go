9027c56a56bc3737e5dec404e720bbfeba8f9bd2
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-15 22:48:59 -0500

    Starting to factor out into circles and rectangles

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -1,9 +1,16 @@
 package geometry
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+// Perimeter returns the perimeter of a rectangle
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// Area returns the area of a rectangle
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
