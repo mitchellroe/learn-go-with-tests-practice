c8befeb7facee712906785dc30f667e5ecfc46e9
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-16 16:00:13 -0500

    You can call out exactly what part of the struct you're defining
    (i.e. name the elements)

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -23,9 +23,9 @@ func TestArea(t *testing.T) {
 		want  float64
 	}{
 		// And here's a list of the ad-hoc structs that go in our slice.
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
