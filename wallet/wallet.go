ca13c99f707c3d39c74be430536da9dc7dd6a136
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-16 21:59:11 -0500

    I wouldn't normally do this, but adding comments to quiet the linter

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -5,22 +5,27 @@ import (
 	"fmt"
 )
 
+// Bitcoin ...
 type Bitcoin int
 
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
+// Wallet ...
 type Wallet struct {
 	balance Bitcoin
 }
 
+// Deposit ...
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+// ErrInsufficientFunds ...
 var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
 
+// Withdraw ...
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.balance {
 		return ErrInsufficientFunds
@@ -30,6 +35,7 @@ func (w *Wallet) Withdraw(amount Bitcoin) error {
 	return nil
 }
 
+// Balance ...
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
