af1af1743359ccf57f86db7ec3ebf0f1fff30999
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-18 21:24:34 -0500

    Refactored so Search is a function of our new Dictionary type

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,8 +1,11 @@
-package main
+package dictionary
 
 import ()
 
+// Dictionary stores a map of terms and their definitions
+type Dictionary map[string]string
+
 // Search through our dictionary for a term
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
