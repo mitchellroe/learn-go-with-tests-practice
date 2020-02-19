d076e37f197ad5fae82bb0d2c91cde35d95c8bcd
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-18 23:49:50 -0500

    The book's solution is a little more tightly coupled, but elegant

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -7,11 +7,13 @@ import (
 // Dictionary stores a map of terms and their definitions
 type Dictionary map[string]string
 
-// ErrNotFound is thrown when we cannot find the definition
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	// ErrNotFound is thrown when we cannot find the definition
+	ErrNotFound = errors.New("could not find the word you were looking for")
 
-// ErrWordExists is thrown when attempting to add a word that's already defined
-var ErrWordExists = errors.New("word already has a definition")
+	// ErrWordExists is thrown when attempting to add a word that's already defined
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 // Search through our dictionary for a term
 func (d Dictionary) Search(word string) (string, error) {
@@ -24,10 +26,17 @@ func (d Dictionary) Search(word string) (string, error) {
 
 // Add adds a definition to the dictionary
 func (d Dictionary) Add(word, definition string) error {
-	if d[word] != "" {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
 		return ErrWordExists
+	default:
+		return err
 	}
-	d[word] = definition
+
 	return nil
 }
 
