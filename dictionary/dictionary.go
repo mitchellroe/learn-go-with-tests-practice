a1d04af6d5f11e084c307f552b1597f09c8bade0
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-18 22:28:48 -0500

    Refactored the error checking

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -7,11 +7,14 @@ import (
 // Dictionary stores a map of terms and their definitions
 type Dictionary map[string]string
 
+// ErrNotFound is thrown when we cannot find the definition
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 // Search through our dictionary for a term
 func (d Dictionary) Search(word string) (string, error) {
 	definition, found := d[word]
 	if !found {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
