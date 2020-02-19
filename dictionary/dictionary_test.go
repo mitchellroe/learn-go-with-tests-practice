c4ba7d5ebc4b542bd5c95d0dc26a273b8cc3062f
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-18 21:25:19 -0500

    Started working on returning errors if word not found

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -1,16 +1,32 @@
-package main
+package dictionary
 
 import (
 	"testing"
 )
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertString(t, err.Error(), want)
+	})
+}
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
