a1d8b173a0b3804ce706c12d97a73cec860c9dd0
Mitchell Roe <mitchell.roe@member.fsf.org>
2020-02-18 22:22:14 -0500

    map lookups return the value followed by a boolean of whether it was found

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,11 +1,17 @@
 package dictionary
 
-import ()
+import (
+	"errors"
+)
 
 // Dictionary stores a map of terms and their definitions
 type Dictionary map[string]string
 
 // Search through our dictionary for a term
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, found := d[word]
+	if !found {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
